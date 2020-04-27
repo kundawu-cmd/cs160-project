57122cbedbae6bb103f4b3d4403dc5b3da555114
Kunda Wu <54656648+kundawu-cmd@users.noreply.github.com>
2020-04-26 20:12:21 -0700

    Change from react to laravel

--- client/src/page/P404.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function P404() {
-    return (
-        <div style={{fontSize: 80, fontFamily: 'QuickSand', paddingTop: 40, paddingLeft: 40, color: "blue"}}>
-            OP! 404! ==> Page Not Found
-        </div>
-    )
-}
